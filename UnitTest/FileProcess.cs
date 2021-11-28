bc0e7b4bf6e0be14f9c4a6a7d39dcfcb8546e9a0
Ericson <Ericson.id@gmail.com>
2021-11-28 12:00:32 -0300

    UnitTest First Deploy

---
+++ UnitTest/FileProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace UnitTest
+{
+    class FileProcess
+    {
+        public  bool FileExist(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+            return File.Exists(fileName);
+        }
+    }
+}
